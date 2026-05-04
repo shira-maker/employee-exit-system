3c23a11b6d35d1e3bf1041a9e2878057a6237d97
Shira Mizrahi <shira@hanikayoncoop.co.il>
2026-05-04 14:51:29 +0300

    add firebase auth config

---
+++ auth-config.js
@@ -0,0 +1,6 @@
+window.EMEL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBTWq7H1a_lzwU-MA5fxJ9iDwFFQAdaIUg",
+  authDomain: "employee-exit-system-db8d9.firebaseapp.com",
+  projectId: "employee-exit-system-db8d9",
+  appId: "1:757764050976:web:20d9da7d4e36e13cd92c45"
+};
