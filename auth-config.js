ff05f992aeca235d944c39e8e04ed002fc1b89a2
Shira Mizrahi <shira@hanikayoncoop.co.il>
2026-05-04 15:13:36 +0300

    fix firebase config

--- auth-config.js
+++ auth-config.js
@@ -1,6 +1,10 @@
 window.EMEL_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBTWq7H1a_lzwU-MA5fxJ9iDwFFQAdaIUg",
-  authDomain: "employee-exit-system-db8d9.firebaseapp.com",
-  projectId: "employee-exit-system-db8d9",
-  appId: "1:757764050976:web:20d9da7d4e36e13cd92c45"
+  apiKey: "AIzaSyDWO5vs4sfQIcBKkT0m9X6dmXm_yoBQ5U8",
+  authDomain: "employee-exit-system-ba003.firebaseapp.com",
+  projectId: "employee-exit-system-ba003",
+  storageBucket: "employee-exit-system-ba003.firebasestorage.app",
+  messagingSenderId: "961204679609",
+  appId: "1:961204679609:web:f23d4d057495cfbfba3c68"
 };
+
+window.EMEL_ALLOWED_EMAIL_DOMAIN = "";
